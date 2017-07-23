bc7fbe8c8978757527403494a4b709045b451fdc
jk1dd <portajonk@gmail.com>
2017-07-22 22:44:11 -0600

    add validations to org model, change rake task to continue creation, only return one of each name

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,8 @@
 class Organization < ApplicationRecord
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :county, presence: true
+  validates :state, presence: true
+  validates :zip, presence: true
+  validates :loc, presence: true
 end
